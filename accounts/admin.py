772891b841b2991f9f7c5378d69039e42df828d1
unknown <shefeqa24@gmail.com>
2023-06-05 17:25:45 +0400

    updated user admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,7 +10,7 @@ class CustomUserAdmin(UserAdmin):
     fieldsets_personal = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('first_name', 'last_name', 'phone_number', 'birthday')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_personal')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_personal','is_business')}),
         (_('Important dates'), {'fields': ('date_joined',)}),
     )
     fieldsets_business = (
@@ -22,7 +22,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets_personal = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name', 'phone_number', 'birthday', 'password'),
+            'fields': ('email', 'first_name', 'last_name', 'phone_number', 'birthday', 'password','is_business'),
         }),
     )
     add_fieldsets_business = (
@@ -63,6 +63,25 @@ class CustomUserAdmin(UserAdmin):
             return self.list_display_business
         else:
             return self.list_display_personal
+        
+    # def add_view(self, request, extra_context=None):
+    #     request.POST._mutable = True
+    #     is_business = request.POST.get('is_business')
+    #     if is_business:
+    #        self.fieldsets = self.add_fieldsets_business
+    #     else:
+    #        self.fieldsets = self.add_fieldsets_personal
+    #     return super().add_view(request, extra_context=extra_context)
+    
+
+    def add_view(self, request, extra_context=None):
+        if 'is_business' in request.POST:
+            self.fieldsets = self.add_fieldsets_business
+        else:
+            self.fieldsets = self.add_fieldsets_personal
+        return super().add_view(request, extra_context=extra_context)
+
+   
 
 
     def save_model(self, request, obj, form, change):
